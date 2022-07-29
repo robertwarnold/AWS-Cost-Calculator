a4478997bacea1e91ae15ed262faf18aa29f8cc9
robertwarnold <95994272+robertwarnold@users.noreply.github.com>
2022-07-29 14:43:41 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 import urllib.request
 
-__author__ = 'Anubhav Mishra'
-__author_email__ = 'anubhavmishra@me.com'
+__author__ = 'Robert Arnold'
+__author_email__ = 'robertwarnold13@gmail.com'
 __versioninfo__ = (0, 0, 9)
 __version__ = '.'.join(map(str, __versioninfo__))
